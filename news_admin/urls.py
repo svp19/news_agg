2ef27d6aa4b62c0d0013a30aae0c14ab208e38c0
Dracula-42 <shrinivasrameshhireraddi@gmail.com>
2019-11-01 01:14:01 +0530

    Created Tag form and ArticleURL form

--- news_admin/urls.py
+++ news_admin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, reverse
 from . import views
-from .views import TopicCreateView
+from .views import TopicCreateView, TagCreateView, UrlCreateView
 
 
 urlpatterns = [
     path('topic/new/', TopicCreateView.as_view(template_name="news_admin/topic_form.html"), name='topic-create'),
+    path('tag/new/', TagCreateView.as_view(template_name="news_admin/tag_form.html"), name='tag-create'),
+    path('url/new/', UrlCreateView.as_view(template_name="news_admin/article_url_form.html"), name='url-goto'),
 ]
