82d88dcc14d84876b9a35c793a224086f10109d8
svp19 <170010025@iitdh.ac.in>
2019-10-31 22:35:41 +0530

    update models'

--- news/models.py
+++ news/models.py
@@ -3,19 +3,19 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Article(models.Model):
     headline = models.CharField(max_length=100)
     location = models.CharField(max_length=255)
     datetime = models.DateTimeField(default=timezone.now())
-    byline = models.CharField(max_length=150)
+    byline = models.CharField(max_length=150, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image_url = models.URLField(max_length=200)
     content = models.CharField(max_length=1000)
-
-
-class Topic(models.Model):
-    name = models.CharField(max_length=100)
-    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article_topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
