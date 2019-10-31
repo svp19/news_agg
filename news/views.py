2a49bd5fd14b646d5b266cbfba470dad8cf86618
svp19 <170010025@iitdh.ac.in>
2019-10-31 20:31:01 +0530

    login required

--- news/views.py
+++ news/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from .query import *
 
 
-# Create your views here.
+@login_required
 def home(request):
-    articles = Article.objects.all()
+    articles = Article.objects.raw(q_all)
+    print(articles)
     return render(request, 'news/home.html', {'articles': articles})
 
 
+@login_required
 def search(request):
     return HttpResponse('Search Results')
