85546bf4e25e5a5c2ef99bf98a660e053d4f59a1
svp19 <170010025@iitdh.ac.in>
2019-10-31 23:28:43 +0530

    urls for users

--- news_agg/urls.py
+++ news_agg/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('news.urls'))
+    path('news/', include('news.urls')),
+    path('users/', include('users.urls'))
 ]
