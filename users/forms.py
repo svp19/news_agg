03646a881a285b09e2944ea10730398b6cf5ca28
avneet5 <170010029@iitdh.ac.in>
2019-11-05 18:59:33 +0530

    Created users/profile

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-# from .models import Profile
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -20,4 +20,4 @@ class UserUpdateForm(forms.ModelForm):
 # class ProfileUpdateForm(forms.ModelForm):
 # 	class Meta:
 # 		model = Profile
-# 		fields = ['image']
+# 		fields = ['image'] 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm
 
 # Create your views here.
 def register(request):
@@ -16,24 +16,25 @@ def register(request):
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
-# @login_required
-# def profile(request):
-# 	if request.method == 'POST':
-# 		u_form = UserUpdateForm(request.POST, instance=request.user)
+@login_required
+def profile(request):
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
 # 		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
-# 		if u_form.is_valid() and p_form.is_valid():
-# 			u_form.save()
+		if u_form.is_valid(): 
+        # and p_form.is_valid():
+			u_form.save()
 # 			p_form.save()
-# 			messages.success(request, f'Your account has been updated!!')
-# 			return redirect('profile')
+			messages.success(request, f'Your account has been updated!!')
+			return redirect('profile')
 
-# 	else:
-# 		u_form = UserUpdateForm(instance=request.user)
+	else:
+		u_form = UserUpdateForm(instance=request.user)
 # 		p_form = ProfileUpdateForm(instance=request.user.profile)
 
-# 	context = {
-# 		'u_form': u_form,
+	context = {
+		'u_form': u_form,
 # 		'p_form': p_form
-# 	}
-# 	return render(request, 'users/profile.html', context)
+	}
+	return render(request, 'users/profile.html', context)
